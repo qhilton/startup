c7a83e197117cea85aa1b39508aa0008cf7ea8f1
Quintin Hitlon <qhilton@byu.edu>
2025-02-13 19:35:30 -0700

    convert viewRecipe

--- recipebook260-html/src/viewRecipe/viewRecipe.jsx
+++ recipebook260-html/src/viewRecipe/viewRecipe.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './viewRecipe.css';
 
 export function ViewRecipe() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>view displayed here</div>
+    <main className="flex flex-col bg-secondary">
+      <h1
+        id="name" aria-placeholder="Name">Recipe Name
+      </h1>
+      <h3>Ingredients</h3>
+      <p id="ingredients"></p>
+      <h3>Instructions</h3>
+      <p id="instructions"></p>
     </main>
   );
 }
