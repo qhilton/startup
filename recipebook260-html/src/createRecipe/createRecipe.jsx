652f46463fbc56be61da308194e0c9657acbb713
Quintin Hitlon <qhilton@byu.edu>
2025-02-13 21:33:29 -0700

    convert createRecipe

--- recipebook260-html/src/createRecipe/createRecipe.jsx
+++ recipebook260-html/src/createRecipe/createRecipe.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './createRecipe.css';
 
 export function CreateRecipe() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>create recipe displayed here</div>
+    <main className="flex flex-col bg-secondary">
+        <h1>
+            Create Recipe
+        </h1>
+        <div className="flex flex-col">
+            <div className="flex">
+              <input type="text" placeholder="name"/>
+              <form>
+                  <label for="ingredients">Ingredients:</label><br></br>
+                  <textarea id="ingredients" name="ingredients" rows="4" cols="50"></textarea>
+              </form>
+            </div>
+            <div className="flex">
+              <form>
+                  <label for="instructions">Instructions:</label><br></br>
+                  <textarea id="instructions" name="instructions" rows="8" cols="50"></textarea>
+              </form>
+            </div>
+        </div>
+        <form method="get" action="viewRecipe.html">
+            <button type="submit" className="btn btn-primary">Save</button>
+        </form>
     </main>
   );
 }
--- recipebook260-html/src/login/login.jsx
+++ recipebook260-html/src/login/login.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="main container-fluid bg-secondary text-center">
+      <div>
+        <h1 className="mb-3">Welcome to Recipe Book</h1>
+        <form method="get" action="home.html">
+          <div className="input-group mb-3">
+            <input className="form-control" type="text" placeholder="username" />
+          </div>
+          <div className="input-group mb-3">
+            <input className="form-control" type="password" placeholder="password" />
+          </div>
+          <button type="submit" className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-secondary">Create</button>
+        </form>
+      </div>
     </main>
   );
 }
