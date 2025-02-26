928f1aa48d07c79ca5c2574ab5cdf08db8129b4e
Quintin Hitlon <qhilton@byu.edu>
2025-02-25 19:43:12 -0700

    add react to create button

--- recipebook260-html/src/home/home.jsx
+++ recipebook260-html/src/home/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import './home.css';
 
 export function Home() {
-  return (
-    <div className="flex flex-1 flex-col">
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex flex-1 flex-col">
             <main className="flex bg-secondary">
                 <div className="flex flex-col left-item">
                     <h1>
@@ -23,13 +27,18 @@ export function Home() {
                         Your Recipes
                     </h1>
 
-
                     <div className="d-flex flex-row">
                         <input className="flex left-item" type="text" placeholder="Search"></input>
 
-                        <form className="flex" method="get" action="createRecipe.html">
+                        {/* <form className="flex" method="get" action="createRecipe.html">
                             <button type="submit" className="btn btn-primary">Create New Recipe</button>
-                        </form>
+                        </form> */}
+                        {/* <li className="nav-item">
+                            <NavLink className='nav-link' to='createRecipe'>Home</NavLink>
+                        </li> */}
+                        <Button variant='primary' onClick={() => navigate('/createRecipe')}>
+                            Create Recipe
+                        </Button>
                     </div>
                     <div>Dinner Rolls</div>
                     <div>Mashed Potatoes</div>
@@ -40,5 +49,5 @@ export function Home() {
                 
             </main>
         </div>
-  );
+    );
 }
