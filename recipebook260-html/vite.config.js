796d7fa7ed586153fc6a39b39b3f20cf30a0c04a
Quintin Hitlon <qhilton@byu.edu>
2025-03-27 21:08:22 -0600

    update vite config

--- recipebook260-html/vite.config.js
+++ recipebook260-html/vite.config.js
@@ -5,5 +5,9 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:4000',
     },
+    '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true,
+      },
   },
 });
